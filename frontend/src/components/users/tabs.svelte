dd0cc48c7db0cd766c6e29144aefdf6adbfcaff5
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2024-04-11 18:47:57 +0530

    renamed my-app to frontend

---
+++ frontend/src/components/users/tabs.svelte
@@ -0,0 +1,33 @@
+<div>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#profile" data-bs-toggle="tab">Profile</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#activity" data-bs-toggle="tab">Activity</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#bookmarks" data-bs-toggle="tab">Bookmarks</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#settings" data-bs-toggle="tab">Settings</a>
+        </li>
+    </ul>
+    <div style="margin-top:10px" />
+    <div class="tab-content">
+        <div class="tab-pane active" id="activity">
+            <ul class="nav nav-tabs" id="repoTabs">
+                <li><a class="nav-link active" href="#summary1" data-bs-toggle="tab">Summary</a></li>
+                <li><a class="nav-link" href="#questions" data-bs-toggle="tab">Questions</a></li>
+                <li><a class="nav-link" href="#answers" data-bs-toggle="tab">Answers</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="summary1">Summary Content</div>
+                <div class="tab-pane" id="questions">Questions Content</div>
+                <div class="tab-pane" id="answers">Answers Content</div>
+            </div>
+        </div>
+        <div class="tab-pane" id="profile">Profile</div>
+        <div class="tab-pane" id="bookmarks">Bookmarks</div>
+    </div>
+</div>
